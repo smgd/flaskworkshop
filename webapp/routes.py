23993ce73d6bd3c2dd3c0abcc0943ef1f705ff8d
smgd <soapmangoesdown@gmail.com>
2018-11-28 19:39:48 +0300

    new_db

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,6 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+from webapp.models import db
 from webapp import app
+from webapp.data import articles_base
+
+
+
+POSTGRES = {
+    'user': 'soapman',
+    # 'pw': 'password',
+    'db': 'flaskworkshop',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+articles_dict = articles_base()
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles=articles_dict)
+
+@app.route('/article/<int:id>')
+def article(id):
+    return render_template('article.html', article=articles_dict[id - 1])
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=100)])
+    username = StringField('Username', [validators.Length(min=4, max=30)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
