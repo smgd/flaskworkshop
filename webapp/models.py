39d8939dbecc508971e321b70b5250f809fb0415
smgd <soapmangoesdown@gmail.com>
2018-11-30 21:38:23 +0300

    bug fix

--- webapp/models.py
+++ webapp/models.py
@@ -18,7 +18,7 @@ class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow().strftime('%m %d %Y - %H:%M'))
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
